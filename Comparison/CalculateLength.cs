b39891f94709565f62eb30041b7f843a8eec61f6
unknown <satya-1298>
2023-03-11 20:39:40 +0530

    Lines Equal or not

---
+++ Comparison/CalculateLength.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comparison
+{
+    public class CalculateLength
+    {
+        int x1, x2, y1, y2;
+        public CalculateLength(int x1, int x2, int y1, int y2)
+        {
+            this.x1 = x1;
+            this.x2 = x2;
+            this.y1 = y1;
+            this.y2 = y2;
+
+        }
+        public double calculate()
+        {
+            double length = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            Console.WriteLine("the length is :" + length);
+            return length;
+        }
+        public void Compare(double line1,double line2)
+        {
+            
+            if (line1.Equals(line2))
+            {
+                Console.WriteLine("This two lines are equal");
+            }
+            else 
+            {
+                Console.WriteLine("two lines are not equal");
+            }
+
+        }
+    }
+}
+
--- Comparison/Program.cs
+++ Comparison/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Comparison
@@ -11,6 +12,20 @@ namespace Comparison
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Computation Program");
+            Console.WriteLine("Enter X1:");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter y1 :");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter x2:");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter y2:");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            CalculateLength clength = new CalculateLength(x1, x2, y1, y2);
+            clength.calculate();
+            double line1= clength.calculate();
+            CalculateLength clengths= new CalculateLength(3,4,5,6);
+            double line2= clengths.calculate();
+            clengths.Compare(line1,line2);
         }
     }
 }
