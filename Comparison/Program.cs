8d132ad1f9b8e0ed915b54c7eff9476d0db57c81
unknown <satya-1298>
2023-03-11 16:19:09 +0530

    Caluculating lenght

--- Comparison/Program.cs
+++ Comparison/Program.cs
@@ -11,6 +11,18 @@ namespace Comparison
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Computation Program");
+            Console.WriteLine("Enter X1:");
+            int x1 =Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter y1 :");
+            int y1 =Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter x2:");
+            int x2 =Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter y2:");
+            int y2=Convert.ToInt32(Console.ReadLine());
+            CalculateLength clength = new CalculateLength(x1,x2,y1,y2);
+            
+            clength.calculate();
+
         }
     }
 }
